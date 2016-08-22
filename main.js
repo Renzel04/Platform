4b47d8bba5c13f14dcc017d96d9ff968f847e837
Renzel04 <Kagitingan04>
2016-08-22 13:07:38 +1000

    error expecting this ";"
    I did what  the tutorial ask but i got this error

--- main.js
+++ main.js
@@ -11,15 +11,15 @@ function getDeltaTime() {
 	endFrameMillis = startFrameMillis;
 	startFrameMillis = Date.now();
 
-		// Find the delta time (dt) - the change in time since the last drawFrame
-		// We need to modify the delta time to something we can use.
-		// We want 1 to represent 1 second, so if the delta is in milliseconds
-		// we divide it by 1000 (or multiply by 0.001). This will make our 
-		// animations appear at the right speed, though we may need to use
-		// some large values to get objects movement and rotation correct
+	// Find the delta time (dt) - the change in time since the last drawFrame
+	// We need to modify the delta time to something we can use.
+	// We want 1 to represent 1 second, so if the delta is in milliseconds
+	// we divide it by 1000 (or multiply by 0.001). This will make our 
+	// animations appear at the right speed, though we may need to use
+	// some large values to get objects movement and rotation correct
 	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
 
-		// validate that the delta is within range
+	// validate that the delta is within range
 	if (deltaTime > 1)
 		deltaTime = 1;
 
@@ -31,9 +31,9 @@ function getDeltaTime() {
 var SCREEN_WIDTH = canvas.width;
 var SCREEN_HEIGHT = canvas.height;
 
-var LAYER_COUNT =3;
-var MAP = {tw: 60, th: 15};
-var TILE =35;
+var LAYER_COUNT = 3;
+var MAP = { tw: 60, th: 15 };
+var TILE = 35;
 var TILESET_TILE = TILE * 2;
 var TILESET_PADDING = 2;
 var TILESET_SPACING = 2;
@@ -54,14 +54,79 @@ chuckNorris.src = "hero.png";
 var player = new Player();
 var keyboard = new Keyboard();
 
+
+var cells = [];  // the array that holds our simplified collision data 
+function initialize() {
+	for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {  // initialize the collision map         
+		cells[layerIdx] = [];
+		var idx = 0;
+		for (var y = 0; y < level1.layers[layerIdx].height; y++) {
+			cells[layerIdx][y] = [];
+			for (var x = 0; x < level1.layers[layerIdx].width; x++) {
+				if (level1.layers[layerIdx].data[idx] != 0) {
+					// for each tile we find in the layer data, we need to create 4 collisions                         
+					// (because our collision squares are 35x35 but the tile in the                          
+					// level are 70x70)                    
+					cells[layerIdx][y][x] = 1;
+					cells[layerIdx][y - 1][x] = 1;
+					cells[layerIdx][y - 1][x + 1] = 1;
+					cells[layerIdx][y][x + 1] = 1;
+				}
+				else if (cells[layerIdx][y][x] != 1) {
+					// if we haven't set this cell's value, then set it to 0 now                     
+					cells[layerIdx][y][x] = 0;
+				}
+				idx++;
+			}
+		}
+	}
+}
+
 // load the image to use for the level tiles 
-var tileset = document.createElement("img"); 
+var tileset = document.createElement("img");
 tileset.src = "tileset.png";
 
 //var enemy = document.creatElement("img");
 var enemy = new Enemy();
 
-function drawMap() {
+
+function cellAtPixelCoord(layer, x, y) 
+{
+	if (x < 0 || x > SCREEN_WIDTH || y < 0)
+		return 1;
+	// let the player drop of the bottom of the screen (this means death)
+	if (y > SCREEN_HEIGHT)
+		return 0;
+	return cellAtTileCoord(layer, p2t(x), p2t(y));
+};
+function cellAtTileCoord(layer, tx, ty) 
+{
+	if (tx < 0 || tx >= MAP.tw || ty < 0)
+		return 1;
+	// let the player drop of the bottom of the screen (this means death)
+	if (ty >= MAP.th)
+		return 0;
+	return cells[layer][ty][tx];
+};
+function tileToPixel(tile) 
+{
+	return tile * TILE;
+};
+function pixelToTile(pixel) 
+{
+	return Math.floor(pixel / TILE);
+};
+function bound(value, min, max) 
+{
+	if (value < min)
+		return min;
+	if (value > max)
+		return max;
+	return value;
+}
+
+function drawMap() 
+{
 	for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
 		var idx = 0; for (var y = 0; y < level1.layers[layerIdx].height; y++) {
 			for (var x = 0; x < level1.layers[layerIdx].width; x++) {
@@ -104,6 +169,12 @@ function run() {
 	context.fillText("FPS: " + fps, 5, 20, 100);
 }
 
+funtion run()
+{
+
+}
+
+intilize();
 
 //-------------------- Don't modify anything below here
 
@@ -111,24 +182,24 @@ function run() {
 // This code will set up the framework so that the 'run' function is called 60 times per second.
 // We have a some options to fall back on in case the browser doesn't support our preferred method.
 (function () {
-  var onEachFrame;
-  if (window.requestAnimationFrame) {
-    onEachFrame = function (cb) {
-      var _cb = function () { cb(); window.requestAnimationFrame(_cb); }
-      _cb();
-    };
-  } else if (window.mozRequestAnimationFrame) {
-    onEachFrame = function (cb) {
-      var _cb = function () { cb(); window.mozRequestAnimationFrame(_cb); }
-      _cb();
-    };
-  } else {
-    onEachFrame = function (cb) {
-      setInterval(cb, 1000 / 60);
-    }
-  }
+	var onEachFrame;
+	if (window.requestAnimationFrame) {
+		onEachFrame = function (cb) {
+			var _cb = function () { cb(); window.requestAnimationFrame(_cb); }
+			_cb();
+		};
+	} else if (window.mozRequestAnimationFrame) {
+		onEachFrame = function (cb) {
+			var _cb = function () { cb(); window.mozRequestAnimationFrame(_cb); }
+			_cb();
+		};
+	} else {
+		onEachFrame = function (cb) {
+			setInterval(cb, 1000 / 60);
+		}
+	}
 
-  window.onEachFrame = onEachFrame;
+	window.onEachFrame = onEachFrame;
 })();
 
 window.onEachFrame(run);
