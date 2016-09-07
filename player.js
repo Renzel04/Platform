9b6ed6893ff8057aaad642aa6fb8e415a4feac99
Renzel04 <Kagitingan04>
2016-09-07 12:34:59 +1000

    Add and fail
    Having a problem in my screen

--- player.js
+++ player.js
@@ -1,19 +1,57 @@
+var LEFT = 0;
+var RIGHT = 1;
+
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_MAX = 6;
+
 var Player = function () {
-    this.image = document.createElement("img");
+    this.sprite = new Sprite("ChuckNorris.png");
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [0, 1, 2, 3, 4, 5, 6, 7]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [8, 9, 10, 11, 12]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [52, 53, 54, 55, 56, 57, 58, 59]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [60, 61, 62, 63, 64]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+        [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+    for (var i = 0; i < ANIM_MAX; i++) {
+        this.sprite.setAnimationOffset(i, -55, -87);
+    }
+    /*this.image = document.createElement("img");*/
     this.position = new Vector2();
     this.position.set(9 * TILE, 0 * TILE);
+
     this.width = 159;
     this.height = 163;
-    this.offset = new Vector2();
-    this.offset.set(-55, -87);
+
+    /*this.offset = new Vector2();
+    this.offset.set(-55, -87);*/
+
     this.velocity = new Vector2();
+
+
     this.falling = true;
     this.jumping = false;
-    this.image.src = "hero.png";
+
+    this.direction = LEFT;
+
+    /*this.image.src = "hero.png";*/
 };
 
-Player.prototype.update = function (deltaTime) {
-    var left = false;
+/*sprite.update()*/
+Player.prototype.update = function (deltaTime) 
+{
+    this.sprite.update(deltaTime);
+    /*var left = false;
     var right = false;
     var jump = false;
     // check keypress events
@@ -41,7 +79,7 @@ Player.prototype.update = function (deltaTime) {
         ddx = ddx - FRICTION; // player was going right, but not any more
     if (jump && !this.jumping && !falling) {
         ddy = ddy - JUMP; // apply an instantaneous (large) vertical impulse
-        this.jumping = true;
+        this.jumping = true;*/
     }
     // calculate the new position and velocity:
     this.position.y = Math.floor(this.position.y + (deltaTime * this.velocity.y));
@@ -87,7 +125,8 @@ Player.prototype.update = function (deltaTime) {
         }
     }
     else if (this.velocity.y < 0) {
-        if ((cell && !celldown) || (cellright && !celldiag && nx)) {
+        if ((cell && !celldown) || (cellright && !celldiag && nx)) 
+        {
             // clamp the y position to avoid jumping into platform above
             this.position.y = tileToPixel(ty + 1);
             this.velocity.y = 0; // stop upward velocity
@@ -97,25 +136,30 @@ Player.prototype.update = function (deltaTime) {
             ny = 0; // player no longer overlaps the cells below
         }
     }
-    if (this.velocity.x > 0) {
-        if ((cellright && !cell) || (celldiag && !celldown && ny)) {
+    if (this.velocity.x > 0) 
+    {
+        if ((cellright && !cell) || (celldiag && !celldown && ny)) 
+        {
             // clamp the x position to avoid moving into the platform we just hit
             this.position.x = tileToPixel(tx);
             this.velocity.x = 0; // stop horizontal velocity
         }
     }
-    else if (this.velocity.x < 0) {
-        if ((cell && !cellright) || (celldown && !celldiag && ny)) {
+    else if (this.velocity.x < 0) 
+    {
+        if ((cell && !cellright) || (celldown && !celldiag && ny)) 
+        {
             // clamp the x position to avoid moving into the platform we just hit
             this.position.x = tileToPixel(tx + 1); this.velocity.x = 0; // stop horizontal velocity
         }
     }
-}
+
     Player.prototype.draw = function () 
     {
         context.save();
         context.translate(this.position.x, this.position.y);
         context.rotate(this.rotation);
         context.drawImage(this.image, -this.width / 2, -this.height / 2);
+        this.sprite.draw(context, this.position.x, this.position.y);
         context.restore();
     }
